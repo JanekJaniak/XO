7fa959e9a611ce929c1cfa79b447eca4fdbd121d
Janek_Janiak <janek.janiak@gmail.com>
2021-03-04 22:00:20 +0100

    Create board WIP

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const board = document.getElementById("game-board");
+const board = document.getElementsByClassName("game-board");
 
+let grid = [
+  ['','',''],
+  ['','',''],
+  ['','','']
+]
+
+console.log(grid);
 console.log(board);
