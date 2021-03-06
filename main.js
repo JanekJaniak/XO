3a40cf2a84868a6968a442499315f78efc3de00b
Janek_Janiak <janek.janiak@gmail.com>
2021-03-06 18:19:14 +0100

    Add renderGrid function

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const board = document.querySelector('[data-board]')
-let grid = [] 
 
-function createGrid() {
-  
+let grid = []
+let tileNumber = 9 
+
+const renderGrid = () => {
+  let i;
+  for(i=0; i<tileNumber; i++) {
+    let tile = document.createElement('div');
+    tile.innerHTML = '';
+    tile.dataset.id = i;
+    tile.className = 'tile'
+    grid.push(tile);
+    board.appendChild(tile) 
+  }
 }
 
-console.log(grid);
-console.log(board);
-console.log(createGrid)
+renderGrid()
+
